3332f61cafe8612afe576066f04a40418d2a509e
hdelc02 <hdelc02@gmail.com>
2020-12-19 17:41:30 -0500

    Point array getter fixed

--- src/stainedGlass/Engine.java
+++ src/stainedGlass/Engine.java
@@ -19,27 +19,47 @@ public class Engine extends PApplet {
     }
 
     public void setup() {
-        PImage transformed = transform(input);
+        //PImage transformed = transform(input);
         background(input);
+        Point[] points = getPoints(input);
+        for(int i=0; i<points.length; i++)
+        {
+            System.out.println(points[i]);
+        }
     }
 
     public void draw() {
 
     }
 
-    public PImage transform(PImage image) {
-
-    }
+//    public PImage transform(PImage image) {
+//
+//    }
 
     public Point[] getPoints(PImage image) {
         final int GRID_SIZE = 10;
         Point[] output = new Point[GRID_SIZE*GRID_SIZE];
-        double ratio = image.height/image.width;
+        double ratio = (double)image.height/(double)image.width;
+        int x, y;
         for(int i=0; i<output.length; i++) {
             double angle = Math.random()*2*Math.PI;
-            double range = ((Math.pow(Math.cos(angle), 2) + ratio*Math.pow(Math.sin(angle), 2)))*(image.width/(GRID_SIZE*2));
+            double range = Math.sqrt(((Math.pow(Math.cos(angle), 2) + ratio*Math.pow(Math.sin(angle), 2))))*((double)image.width/(double)((GRID_SIZE-1)*2));
             double magnitude = Math.random()*range;
-            output[i] = new Point((int)(magnitude*Math.cos(angle)), (int)(magnitude*Math.sin(angle)));
+            x = (int)((magnitude*Math.cos(angle)) + (i%GRID_SIZE)*((double)image.width/(double)(GRID_SIZE-1)));
+            y = (int)((magnitude*Math.sin(angle)) + (i/GRID_SIZE)*((double)image.height/(double)(GRID_SIZE-1)));
+            if(i<GRID_SIZE) {
+                y = 0;
+            }
+            else if(i/GRID_SIZE == GRID_SIZE-1) {
+                y = image.height-1;
+            }
+            if(i%GRID_SIZE == 0) {
+                x = 0;
+            }
+            else if(i%GRID_SIZE == GRID_SIZE-1) {
+                x = image.width-1;
+            }
+            output[i] = new Point(x, y);
         }
         return output;
     }
