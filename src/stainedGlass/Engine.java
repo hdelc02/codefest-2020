74f8d3757661b1d26c75f7598e1d0fd377617ed3
hdelc02 <hdelc02@gmail.com>
2020-12-19 18:31:22 -0500

    Added nearPoints()

--- src/stainedGlass/Engine.java
+++ src/stainedGlass/Engine.java
@@ -4,6 +4,9 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Engine extends PApplet {
 
@@ -55,6 +58,45 @@ public class Engine extends PApplet {
                 x = image.width-1;
             }
             output[i] = new Point(x, y);
+        }
+        return output;
+    }
+
+    public ArrayList<Point> contourPoints(PImage image) {
+        ArrayList<Point> basePoints = new ArrayList<Point>();
+        Collections.addAll(basePoints, getPoints(image));
+        ArrayList<Point> points = reducePoints(image, basePoints, 3);
+        return points;
+    }
+
+    private ArrayList<Point> reducePoints(PImage image, ArrayList<Point> points, int iteration) {
+
+    }
+
+    public Point[] nearPoints(Point origin, ArrayList<Point> points, int numPoints) {
+        Point[] output = new Point[numPoints];
+        float[] dists = new float[numPoints];
+        ArrayList<Point> pointsCopy = new ArrayList<Point>(points);
+        pointsCopy.remove(origin);
+        int maxDist = 0;
+        for(int i=0; i<pointsCopy.size(); i++) {
+            if(i<numPoints) {
+                output[i] = pointsCopy.get(i);
+                dists[i] = dist(pointsCopy.get(i).x, pointsCopy.get(i).y, origin.x, origin.y);
+                if(dists[maxDist]<=dists[i])
+                    dists[maxDist] = dists[i];
+            }
+            else {
+                float thisDist = dist(pointsCopy.get(i).x, pointsCopy.get(i).y, origin.x, origin.y)
+                if(thisDist < dists[maxDist]) {
+                    dists[maxDist] = thisDist;
+                    output[maxDist] = pointsCopy.get(i);
+                    for(int j=0; j<numPoints; j++) {
+                        if(dists[j]>maxDist)
+                            maxDist = j;
+                    }
+                }
+            }
         }
         return output;
     }
