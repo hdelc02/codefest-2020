4beb401a07bbdfd589fd7b8cd24f05d55072bede
hdelc02 <hdelc02@gmail.com>
2020-12-19 15:56:25 -0500

    First commit

--- src/stainedGlass/Engine.java
+++ src/stainedGlass/Engine.java
@@ -1,15 +1,23 @@
 package stainedGlass;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Engine extends PApplet {
 
+    PImage input;
+
     public static void main(String[] args) {
-        PApplet.main("Engine");
+        PApplet.main("stainedGlass.Engine");
+    }
+
+    public void settings() {
+        input = loadImage("swag cat.jpg");
+        size(input.width, input.height);
     }
 
     public void setup() {
-
+        background(input);
     }
 
     public void draw() {
