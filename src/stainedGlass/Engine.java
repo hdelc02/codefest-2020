fdf8890915eefc86c3452799f5d27807377fd51a
hdelc02 <hdelc02@gmail.com>
2020-12-19 15:44:25 -0500

    First commit

---
+++ src/stainedGlass/Engine.java
@@ -0,0 +1,18 @@
+package stainedGlass;
+
+import processing.core.PApplet;
+
+public class Engine extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Engine");
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
--- src/stainedGlass/main.java
+++
@@ -1,8 +0,0 @@
-package stainedGlass;
-
-public class main {
-
-    public static void main(String[] args) {
-
-    }
-}
